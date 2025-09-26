b87399ee68acbf411f792c62f516b3c2bd4c00b5
NYLONXD <himanshuj19772005@gmail.com>
2025-09-26 07:34:12 +0530

    bcrypt issue resolved by aws dev

--- api/app/utils/security.py
+++ api/app/utils/security.py
@@ -2,7 +2,7 @@ from api.app.config import pwd_context
 
 def hash_password(password: str) -> str:
     """Hash password for storage"""
-    return pwd_context.hash(password)
+    return pwd_context.hash(password, scheme="bcrypt")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify user password"""
